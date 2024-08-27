b53925944e125ee5bd5cc57cda6d9dac661b90f4
aniilcanpolat <canpolatanil8@gmail.com>
2024-08-27 13:54:34 +0300

    Initial Commit

---
+++ MovieList/diteilViewcontroller.swift
@@ -0,0 +1,22 @@
+//
+//  liteairViewcontroller.swift
+//  MovieList
+//
+//  Created by Apple on 21.08.2024.
+//
+
+import Foundation
+import UIKit
+
+class DetailViewController:UIViewController {
+    
+    var selectedIndex:Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("SELECTED INDEX: \(selectedIndex)")
+        var movie:Movie = movieList[selectedIndex]
+        print("SELECTED MOVİE: " + movie.title)
+        
+    }
+}
